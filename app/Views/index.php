6ce81210b0c685db2f112d9ddb99ee96522abb30
LuissDuraann <135404529+LuissDuraann@users.noreply.github.com>
2023-06-02 22:58:54 -0600

    Update index.php

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,8 +1,1 @@
-esto es el inicio de la aplicacion alv holaaaaaaa duran alv
-
-
-
-Quiero veeeer gaaaaa
-.i.
-
-Oyeeeee me atasque a la Odalis jsjsjsjsjsjsjsjsjs puta que rico cara de vergaaaaaa
+esto es el inicio de la aplicacion 
