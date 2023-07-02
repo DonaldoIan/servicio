1047b0f15ab0ab98e80919808df6921e3648dd0c
DonaldoIan <134822940+DonaldoIan@users.noreply.github.com>
2023-07-01 22:53:05 -0600

    Diseño Menu principal

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -1,104 +1,87 @@
 <!DOCTYPE html>
-<html>
+<html  >
 <head>
-  <title>Menú</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <style>
-    .navbar {
-      justify-content: flex-start;
-    }
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.7.12, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="<?=base_url('/public/img/assets');?>/images/imagen5-1.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+  <link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/dropdown/css/style.css">
+  <link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="<?=base_url('/public/img/assets');?>/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="<?=base_url('/public/img/assets');?>/mobirise/css/mbr-additional.css" type="text/css">
 
-    .navbar-nav {
-      margin-left: auto;
-    }
-    .container {
-      display: flex;
-      justify-content: flex-start;
-    }
-
-    .right-align {
-      margin-left: auto;
-    }
-  </style>
+  
+  
+  
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="<?=base_url('menu');?>">Menú</a>
-    </div>
-  </nav>
-
-  <!-- Contenido de la página -->
-  <div class="container">
-    <div class="row row-cols-8">
-      <div class="col">
-        <div class="container">
-          <!--eliminar con margin top (en proceso)-->
-          <br>
-          <br><br><br>
-          <!---->
-          <!--div principal de opciones a elegir (fondo)-->
-          <div id="opciones">
-            <div class="row justify-content-center">
-              <!--Titulo de esta sección "hola administrador"-->
-              <div class="col-8" style="margin-top: 10%;">
-                <h2 id="sub_hola">¡Hola, administrador!</h2>
-              </div>
+  
+  <section data-bs-version="5.1" class="menu cid-tHSxImlL10" once="menu" id="menu1-3">
+    
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.html">
+                        <img src="<?=base_url('/public/img/assets');?>/images/imagen5-1.png" alt="" style="height: 4.4rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-primary display-5" href="index.html#top">BUAP</a></span>
             </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-primary display-4"  data-toggle="modal" data-target="#modalParaEditar"> <span class="mbrib-contact-form mbr-iconfont mbr-iconfont-btn"></span>Registrar Trabajadores</a></li><li class="nav-item"><a class="nav-link link text-primary display-4" href="<?= base_url('tusuarios');?>" aria-expanded="false"><span class="mobi-mbri mobi-mbri-users mbr-iconfont mbr-iconfont-btn"></span>Usuarios Registrados</a></li><li class="nav-item"><a class="nav-link link text-primary display-4" data-toggle="modal" data-target="#modalParaAsistencias"><span class="mobi-mbri mobi-mbri-contact-form mbr-iconfont mbr-iconfont-btn"></span>Tomar Asistencia</a></li><li class="nav-item"><a class="nav-link link text-primary display-4" href="<?= base_url('tasistencia');?>" aria-expanded="false"><span class="mobi-mbri mobi-mbri-bulleted-list mbr-iconfont mbr-iconfont-btn"></span>Tabla de Asistencias</a></li></ul>
 
-            <!--eliminar a futuro-->
-            <br>
-            <!---->
-
-            <!--subtitulo "que va a hacer hoy"-->
-            <div class="row justify-content-center">
-              <div class="col-8">
-                <h4 id="subsub_hola">¿Qué va hacer hoy?</h4>
-              </div>
+                <div class="navbar-buttons mbr-section-btn"><a class="btn btn-success display-4" href="index.html"><span class="mobi-mbri mobi-mbri-logout mbr-iconfont mbr-iconfont-btn"></span>Cerrar Sesión</a></div>
             </div>
-
-            <!--eliminar a futuro-->
-            <br>
-            <br>
-            <!---->
-
-            <!--primera sección de filas-->
-            <div class="row justify-content-around">
-              <!--agregar al banco-->
-              <a href="<?= base_url('tasistencia');?>" class="nav-link btn btn-primary" type="button" style="margin-right: 5px;">Tabla de asistencias</a><br>
-              <!--visualizar avances-->
-              <a href="<?= base_url('tusuarios');?>" class="nav-link btn btn-primary" type="button" style="margin-right: 5px;">Usuarios registrados</a>
-            </div>
-            <!--eliminar a futuro-->
-            <br>
-            <!---->
-
-            <!--segunda sección de filas-->
-            <div class="row justify-content-around">
-              <!--crear evaluación-->
-              <button class="nav-link btn btn-primary" data-toggle="modal" data-target="#modalParaEditar">Registrar Trabajadores</button><br>
-              <!--gestionar estudiantes-->
-              <button class="nav-link btn btn-primary" data-toggle="modal" data-target="#modalParaAsistencias">Tomar asistencia</button>
-            </div>
-            <!--eliminar a futuro-->
-            <br>
-
-          </div>
         </div>
-      </div>
-      <!--imagen de logo principal en pantalla-->
-      <div class="col">
-        <a style="display: flex; justify-content: center; margin-top: 10%;"><img src="<?=base_url()?>public/img/logo.png" alt=""></a>
-      </div>
-    </div>
-  </div>
+    </nav>
 
-  <!-- Modal para agregar un nuevo trabajador -->
-  <br>
+</section>
+
+
+<section data-bs-version="5.1" class="header10 cid-tIOWxVISMG mbr-fullscreen" id="header10-7">
+
+
+    <div class="align-center container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-9">
+                <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>¡BIENVENIDO ADMINISTRADOR!</strong></h1>
+                <p class="mbr-text mbr-fonts-style display-5">Recuerda que todas las herramientas de trabajo se encuentran disponibles en el menú de la parte superior.</p>
+                <div class="mbr-section-btn mt-3"><a class="btn btn-success display-4" href="index.html">Tutorial de Uso</a></div>
+                <div class="image-wrap mt-4">
+                    <img src="<?=base_url('/public/img/assets');?>/images/logo-de-la-buap.svg-3-600x600.png" alt="" title="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section><script src="<?=base_url('/public/img/assets');?>/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="<?=base_url('/public/img/assets');?>/smoothscroll/smooth-scroll.js"></script>  <script src="<?=base_url('/public/img/assets');?>/ytplayer/index.js"></script>  <script src="<?=base_url('/public/img/assets');?>/dropdown/js/navbar-dropdown.js"></script>  <script src="<?=base_url('/public/img/assets');?>/theme/js/script.js"></script>  
+  
+  
+
+
+
+
+<!-- Modal para agregar un nuevo trabajador -->
+<br>
   <?php if(session('mensaje')){?>
   <div class="modal fade" id="mensajeModal" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog">
@@ -271,5 +254,20 @@
     });
 
   </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
